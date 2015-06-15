64cafae9b6e56c57769a070f39a0f95b22478539
Lee Curtis <lee.curtis@troven.com.au>
2015-06-15 10:14:33 +1000

    bump version

--- src/static/js/meta4beta/fact.js
+++ src/static/js/meta4beta/fact.js
@@ -447,26 +447,48 @@ fact.DEBUG && console.warn("Parse Error:", that, e);
 			fn: function(v) { return v===false||v?true:false }
 		},
 		"email": {
-			message: "Invalid email address",
+			message: "invalid email address",
 			pattern: /^[\w\-]{1,}([\w\-\+.]{1,1}[\w\-]{1,}){0,}[@][\w\-]{1,}([.]([\w\-]{1,})){1,3}$/
 		},
 		"url": {
-			message: "Invalid Web Address",
+			message: "not valid URL",
 			pattern: /^(http|https):\/\/(([A-Z0-9][A-Z0-9_\-]*)(\.[A-Z0-9][A-Z0-9_\-]*)+)(:(\d+))?\/?/i
 		},
 		"number": {
-			message: "Not a number",
+			message: "Invalid number",
 			pattern: /^[0-9]*\.?[0-9]*?$/
 		},
 		"currency": {
-			message: "Not a number",
+			message: "Invalid number",
 			pattern: /^[0-9]*\.?[0-9]*?$/
 		},
+		"iso8601": {
+			message: "Invalid date time",
+			pattern: /^(19[0-9]{2}|[2-9][0-9]{3})-((0(1|3|5|7|8)|10|12)-(0[1-9]|1[0-9]|2[0-9]|3[0-1])|(0(4|6|9)|11)-(0[1-9]|1[0-9]|2[0-9]|30)|(02)-(0[1-9]|1[0-9]|2[0-9]))\x20(0[0-9]|1[0-9]|2[0-3])(:[0-5][0-9]){2}$/
+		},
+		"strongPassword": {
+			message: "Password too weak. 6+ characters including at least 1 upper or lower alpha, and 1 digit.",
+			pattern: /^(?=.*\d)(?=.*[a-zA-Z])(?!.*[\W_\x7B-\xFF]).{6,64}$/
+		},
+		"creditCard": {
+			message: "Invalid credit card",
+			pattern: /^((4\d{3})|(5[1-5]\d{2})|(6011))-?\d{4}-?\d{4}-?\d{4}|3[4,7]\d{13}$/
+		},
+		"isbn": {
+			message: "Invalid ISBN",
+			pattern: /^ISBN\s(?=[-0-9xX ]{13}$)(?:[0-9]+[- ]){3}[0-9]*[xX0-9]$/
+		},
 		"date": {
+			message: "Invalid date (dd/mm/yyyy)",
+			pattern: /^([0-1][0-9]|[2][0-3]):([0-5][0-9])$/
 		},
 		"time": {
+			message: "Invalid time (hh:mm)",
+			pattern: /^([0-1][0-9]|[2][0-3]):([0-5][0-9])$/
 		},
 		"datetime": {
+			message: "Invalid date / time (dd/mm/yyyy hh:mm)",
+			pattern: /^((((([13578])|(1[0-2]))[\-\/\s]?(([1-9])|([1-2][0-9])|(3[01])))|((([469])|(11))[\-\/\s]?(([1-9])|([1-2][0-9])|(30)))|(2[\-\/\s]?(([1-9])|([1-2][0-9]))))[\-\/\s]?\d{4})(\s((([1-9])|(1[02]))\:([0-5][0-9])((\s)|(\:([0-5][0-9])\s))([AM|PM|am|pm]{2,2})))?$/
 		}
 	}
 
--- sync2node.sh
+++
@@ -1,3 +0,0 @@
-NAME=$1
-rsync -r src/* ../${NAME}/node_modules/meta4ux/src/
-
